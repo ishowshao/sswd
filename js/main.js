82a2f3797c00555f0d144c17f125fcfa180609cf
ishowshao <ishowshao@me.com>
2015-01-11 01:47:20 +0800

    files

--- js/main.js
+++ js/main.js
@@ -3,4 +3,26 @@ Zepto(function () {
         $('#step-1').addClass('hidden');
         $('#step-2').removeClass('hidden');
     });
+    $('#has-judge').on('change', function () {
+        if (this.checked) {
+            $('.judge-setting').removeClass('hidden');
+        } else {
+            alert('您选择的是无法官玩法，只能由系统自动选择平民和卧底的词语');
+            $('.judge-setting').addClass('hidden');
+        }
+    });
+    $('[name=word]').on('change', function () {
+        if (this.value === 'auto') {
+            // ajax
+            $.get('word.php', function (res) {
+                if (res) {
+                    res = JSON.parse(res);
+                    $('#civilian-word').val(res.data.civilian);
+                    $('#spy-word').val(res.data.spy);
+                }
+            });
+        } else {
+            $('#civilian-word,#spy-word').val('');
+        }
+    });
 });
---
+++ word.php
@@ -0,0 +1,6 @@
+<?php
+$response = array(
+    'success' => true,
+    'data' => array('civilian' => '111', 'spy' => '222'),
+);
+echo json_encode($response);
