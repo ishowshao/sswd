f16071faa887d4676bfebd51cd9eddc7d58ad3db
ishowshao <ishowshao@me.com>
2015-01-11 15:50:48 +0800

    create room

--- create.php
+++ create.php
@@ -1,4 +1,60 @@
 <?php
-/**
- * @author ishowshao
- */
+// 数据校验
+$required = array('civilian-count', 'spy-count', 'word-by', 'civilian-word', 'spy-word', 'game-type');
+foreach ($required as $key) {
+    if (!isset($_POST[$key])) {
+        exit;
+    }
+}
+$civilianCount = intval($_POST['civilian-count']);
+$spyCount = intval($_POST['spy-count']);
+$wordBy = trim($_POST['word-by']);
+$civilianWord = trim($_POST['civilian-word']);
+$spyWord = trim($_POST['spy-word']);
+$gameType = trim($_POST['game-type']);
+// 数据存储
+
+// 生成id
+do {
+    $id = rand(1000, 9999);
+} while (file_exists('data/' . $id . '.json'));
+
+$dataFileName = 'data/' . $id . '.json';
+$data = array(
+    'civilian-count' => $civilianCount,
+    'spy-count' => $spyCount,
+    'word-by' => $wordBy,
+    'civilian-word' => $civilianWord,
+    'spy-word' => $spyWord,
+    'game-type' => $gameType,
+);
+file_put_contents($dataFileName, json_encode($data));
+// 写文件
+?><!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=no">
+    <title>谁是卧底游戏助手</title>
+    <style>
+        body {
+            font-family: Helvetica Neue, Helvetica, Arial, sans-serif;
+        }
+        .room-number {
+            font-size: 50px;
+            text-align: center;
+        }
+        p {
+            margin: 8px 0;
+        }
+    </style>
+</head>
+<body>
+<div>
+    <p>房间号如下，请把此页面发送给参与玩家或者让玩家扫二维码</p>
+    <p class="room-number"><?php echo $id ?></p>
+</div>
+<script src="js/zepto.min.js"></script>
+</body>
+</html>
--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ Zepto(function () {
     $('#original,#advanced').on('click', function () {
         $('#step-1').addClass('hidden');
         $('#step-2').removeClass('hidden');
+        $('#game-type').val(this.id);
     });
     $('#has-judge').on('change', function () {
         if (this.checked) {
@@ -11,7 +12,7 @@ Zepto(function () {
             $('.judge-setting').addClass('hidden');
         }
     });
-    $('[name=word]').on('change', function () {
+    $('[name=word-by]').on('change', function () {
         if (this.value === 'auto') {
             // ajax
             $.get('word.php', function (res) {
@@ -23,6 +24,23 @@ Zepto(function () {
             });
         } else {
             $('#civilian-word,#spy-word').val('');
+        }
+    });
+    $('input[type=number],input[type=text]').on('blur', function () {
+        $(this.parentNode)[this.value ? 'removeClass' : 'addClass']('has-error');
+    });
+    $('form [type=submit]').on('click', function (e) {
+        e.preventDefault();
+        var required = 0;
+        $('input[type=number],input[type=text]').each(function () {
+            if (!this.value) {
+                $(this.parentNode).addClass('has-error');
+            } else {
+                required++;
+            }
+        });
+        if (required === 4) {
+            $('form').submit();
         }
     });
 });
