f88013311f7578a9ee451442bb67479e13fba30b
ishowshao <ishowshao@me.com>
2015-01-12 14:24:16 +0800

    test

--- hook.php
+++ hook.php
@@ -2,4 +2,5 @@
 /**
  * @author ishowshao
  */
-file_put_contents('hook.json', print_r($_POST, true));
+file_put_contents('hook.json', print_r($_POST, true), FILE_APPEND);
+file_put_contents('hook.json', print_r($_REQUEST, true), FILE_APPEND);
