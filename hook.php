1bec6d6e6305ab3f7632cee620797bdd02ecb66e
ishowshao <ishowshao@me.com>
2015-01-12 15:13:02 +0800

    test

--- hook.php
+++ hook.php
@@ -2,7 +2,9 @@
 /**
  * @author ishowshao
  */
-$json = file_get_contents('php://input');
-$jsonObj = json_decode($json, true);
-file_put_contents('hook.json', print_r($jsonObj, true));
-file_put_contents('hook.json', print_r($_SERVER, true), FILE_APPEND);
+if (isset($_SERVER['HTTP_USER_AGENT']) && strtolower(substr($_SERVER['HTTP_USER_AGENT'], 0, 6)) === 'github') {
+    $json = file_get_contents('php://input');
+    $jsonObj = json_decode($json, true);
+    file_put_contents('hook.json', print_r($jsonObj, true));
+    file_put_contents('hook.json', print_r($_SERVER, true), FILE_APPEND);
+}
