a0a5638605c1217bf7c6781eb6d4fc100c868a84
ishowshao <ishowshao@me.com>
2015-01-12 14:53:09 +0800

    test

--- hook.php
+++ hook.php
@@ -4,4 +4,5 @@
  */
 $json = file_get_contents('php://input');
 $jsonObj = json_decode($json, true);
-file_put_contents('hook.json', print_r($jsonObj, true), FILE_APPEND);
+file_put_contents('hook.json', print_r($jsonObj, true));
+file_put_contents('hook.json', print_r($_SERVER, true), FILE_APPEND);
